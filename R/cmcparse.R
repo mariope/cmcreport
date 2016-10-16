9715515658f966af010f171a72a5b5aebc0d6bbb
mariope <mario.pisa@gmail.com>
2016-10-16 19:10:49 +0200

    init repository cmcreport

---
+++ R/cmcparse.R
@@ -0,0 +1,13 @@
+#' cmcparse: A package for parse CMC Markets report from exported CSV file.
+#'
+#' The cmcparse package provides three categories of important functions:
+#' readCMCReport, performanceCMCreport and writeCMCreportToCsv.
+#'
+#' @section cmcparse functions:
+#' The foo functions...
+#'
+#' @docType package
+#' @name cmcparse
+#' @author Mario Pisa
+#' @seealso \url{https://www.cmcmarkets.com/}
+NULL
